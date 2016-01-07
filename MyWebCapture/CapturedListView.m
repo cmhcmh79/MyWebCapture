b5bf7921d70856809ea0927f30700cd2aa77fa59
jschoi <dr_ntk@naver.com>
2016-01-07 17:08:10 +0900

    픽커뷰를 사용하여 캡쳐 이미지 정렬 방식 선택

--- MyWebCapture/CapturedListView.m
+++ MyWebCapture/CapturedListView.m
@@ -19,10 +19,25 @@ static const int TAG_CELL_TITLE = 102;
 static const int TAG_CELL_URL   = 103;
 static const int TAG_CELL_DATE  = 104;
 
-@interface CapturedListView ()
+/**
+ * 갭쳐 데이터 정열령을 위한 피커 뷰에 표시할 문자열
+ */
+static NSString * const stringOrder[2][2] = { {@"Time", @"Title"}, {@"Ascending", @"Descending"}};
+
+@interface CapturedListView () <UIPickerViewDataSource, UIPickerViewDelegate>
 
 @property (strong, nonatomic) DataManager *dataManager;
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *buttonOrder;
+
+// 픽커뷰 정의
+@property (strong, nonatomic) UIPickerView *pickerView;
+@property (strong, nonatomic) UIButton *pickerButton;
+
+// 정렬방식 정의
+@property (nonatomic) NSInteger selectedOrder;
+@property (nonatomic) NSInteger selectedDir;
+
 @end
 
 @implementation CapturedListView
@@ -32,9 +47,23 @@ static const int TAG_CELL_DATE  = 104;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    // 정렬 방식 선택
+    self.selectedOrder = 0;
+    self.selectedDir = 1;
+    
     self.dataManager = [DataManager GetSingleInstance];
-    [self.dataManager readCapturedData];
+    [self.dataManager readCapturedDataOrderby:[stringOrder[0][self.selectedOrder] lowercaseString]
+                                withAscending:[stringOrder[1][self.selectedDir] isEqualToString:stringOrder[1][0]]];
+    
+    self.buttonOrder.title = [NSString stringWithFormat:@"Order by %@ %@",
+                              stringOrder[0][self.selectedOrder],
+                              stringOrder[1][self.selectedDir] ];
+
     NSLog("captured count:%li", self.dataManager.capturedDatas.count);
+    
+    
+    // 테이블뷰 높이 설정
+    self.tableView.rowHeight = 100;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -45,8 +74,11 @@ static const int TAG_CELL_DATE  = 104;
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    NSLog();
-    [self.dataManager readCapturedData];
+    
+    [self.dataManager readCapturedDataOrderby:[stringOrder[0][self.selectedOrder] lowercaseString]
+                                withAscending:[stringOrder[1][self.selectedDir] isEqualToString:stringOrder[1][0]]];
+    
+    NSLog("captured count:%li", self.dataManager.capturedDatas.count);
 }
 
 #pragma mark - Navigation
@@ -67,13 +99,15 @@ static const int TAG_CELL_DATE  = 104;
     return self.dataManager.capturedDatas.count;
 }
 
+/*
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     return 100;
 }
+*/
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    //NSLog(@"index-row:%li section:%li", indexPath.row, indexPath.section);
+    NSLog(@"index-row:%li section:%li", indexPath.row, indexPath.section);
     UITableViewCell *cell;
     
     static NSString *cellID = @"CapturedCell";
@@ -111,12 +145,112 @@ static const int TAG_CELL_DATE  = 104;
     
     // 세로 길이로 잘라 표시
     CGImageRef imageRef = CGImageCreateWithImageInRect([image CGImage],
-                                                       CGRectMake(0, 0,
-                                                                  image.size.width,
-                                                                  (heigth < image.size.height) ? heigth: image.size.height));
+                                                       CGRectMake(0, 0, image.size.width, MIN(heigth, image.size.height)));
     imageView.image = [UIImage imageWithCGImage:imageRef];
 
     return cell;
 }
 
+#pragma mark - button action
+- (IBAction)pressedOrderButton:(id)sender {
+    NSLog();
+    
+    // 모든 뷰 상태 disable 만들기
+    for(UIView *subView in self.view.subviews) {
+        subView.userInteractionEnabled = NO;
+        subView.alpha = 0.3;
+    }
+
+    // 픽커뷰 생성
+    self.pickerView = [[UIPickerView alloc] init];
+    self.pickerView.delegate = self;
+    self.pickerView.dataSource = self;
+    
+    self.pickerView.backgroundColor = [UIColor whiteColor];
+    self.pickerView.center = self.view.center;
+    self.pickerView.layer.borderColor = [UIColor grayColor].CGColor;
+    self.pickerView.layer.borderWidth = 1;
+    self.pickerView.layer.cornerRadius = 10;
+    self.pickerView.layer.masksToBounds = YES;
+    
+    [self.view addSubview:self.pickerView];
+    
+    // 현제 설정 선택
+    [self.pickerView selectRow:self.selectedOrder inComponent:0 animated:NO];
+    [self.pickerView selectRow:self.selectedDir inComponent:1 animated:NO];
+    
+    // 버튼 생성
+    self.pickerButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    self.pickerButton.frame = CGRectMake(self.pickerView.frame.origin.x,
+                              self.pickerView.frame.origin.y + self.pickerView.frame.size.height + 10,
+                              self.pickerView.frame.size.width,
+                              30 );
+    
+    [self.pickerButton  setTitle:@"Select Order" forState:UIControlStateNormal];
+    self.pickerButton.layer.borderWidth = 1;
+    self.pickerButton.layer.borderColor = [UIColor grayColor].CGColor;
+    self.pickerButton.layer.cornerRadius = 10;
+    self.pickerButton.layer.masksToBounds = YES;
+    self.pickerButton.backgroundColor = [UIColor whiteColor];
+    [self.pickerButton addTarget:self action:@selector(pressedSelectOrderButton:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.pickerButton];
+}
+
+// 픽커 뷰 선택 완료시
+- (void)pressedSelectOrderButton:(id)sender
+{
+    NSLog();
+    self.selectedOrder = [self.pickerView selectedRowInComponent:0];
+    self.selectedDir = [self.pickerView selectedRowInComponent:1];
+    
+    // 버튼 타이틀 변경
+    self.buttonOrder.title = [NSString stringWithFormat:@"Order by %@ %@",
+                              stringOrder[0][self.selectedOrder],
+                              stringOrder[1][self.selectedDir] ];
+    
+    // 픽커 제거
+    [self.pickerView removeFromSuperview];
+    self.pickerView = nil;
+    
+    // 버튼 제거
+    [self.pickerButton removeFromSuperview];
+    self.pickerButton = nil;
+    
+    // 모든 뷰 활성화
+    for(UIView *subView in self.view.subviews) {
+        subView.userInteractionEnabled = YES;
+        subView.alpha = 1.0;
+    }
+    
+    // 갭쳐 데이터 다시 읽기
+    [self.dataManager readCapturedDataOrderby:[stringOrder[0][self.selectedOrder] lowercaseString]
+                                withAscending:[stringOrder[1][self.selectedDir] isEqualToString:stringOrder[1][0]]];
+    [self.tableView reloadData];
+}
+
+- (IBAction)pressedDeleteButton:(id)sender {
+    NSLog();
+}
+
+#pragma mark - PickerView datasource , delegate
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 2;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return 2;
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    //return [NSString stringWithFormat:@"string %li - %li", row, component];
+    return stringOrder[component][row];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    NSLog(@"row:%li componet:%li", row, component);
+}
 @end
